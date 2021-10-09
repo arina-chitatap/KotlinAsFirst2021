38a13c9c64787dbedb4d8d2813d7c36b014ebcf9
arina-chitatap <yurinskaya.ai@edu.spbstu.ru>
2021-10-09 17:34:38 +0300

    isnumberhappy+daysinmonth

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -18,7 +18,10 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    return if ((number % 10 + (number / 10) % 10) == (number / 1000 + number / 100 % 10)) true
+    else false
+}
 
 /**
  * Простая (2 балла)
@@ -36,7 +39,13 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    return when (month) {
+        1, 3, 5, 7, 8, 10, 12 -> 31
+        2 -> if ((year % 4 == 0) && (year % 100 != 0) or (year % 400 == 0)) 29 else 28
+        else -> 30
+    }
+}
 
 /**
  * Простая (2 балла)
